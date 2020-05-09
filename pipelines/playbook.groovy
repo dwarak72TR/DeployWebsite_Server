19feb9702ecb924fd604956b274d565b76b99865
dwarks123 <47684557+dwarks123@users.noreply.github.com>
2020-05-09 17:22:59 +0530

    Update playbook.groovy

--- pipelines/playbook.groovy
+++ pipelines/playbook.groovy
@@ -1,1 +1,49 @@
-ansible --module-name ping --extra-vars 'ansible_user=domain\user ansible_password=PASSWORD' --inventory 10.10.6.184, all
+def buildAgent = 'packer'
+def playbook = "ansiename.yaml"
+
+timestamps
+{
+    node(buildAgent)
+    {  
+        // stage('Select Env And Invoke Playbook')
+        // {   
+        //     module = "${Envname}"
+        //     switch(module)
+        //     {
+        //         case "QA":  
+        //                     extraVars = "-e \"{'env':'${Envname}','uname':'${Username}'}\" "
+        //                     limit = "${Envname}"
+        //                     break;
+                            
+        //         case "DEV": 
+        //                     extraVars = "-e \"{'env':'${Envname}','uname':'${Username}'}\" "
+        //                     limit = "${Envname}"
+        //                     break;
+                            
+        //         case "PD":
+        //                     extraVars = "-e \"{'env':'${Envname}','uname':'${Username}'}\" "
+        //                     limit = ""
+                            
+        //                     break;
+        //     }
+        //     dir('ansible')
+        //     {
+        //         try{
+        //             ansiblePlaybook(
+        //             playbook: "$playbook",
+        //             extras: "$extraVars",
+        //             limit: "$limit")
+        //         }
+        //     }
+        //     catch (e)
+        //     {
+        //         echo e.getMessage()
+        //         currentBuild.result='FAILURE'
+        //     }   
+        //     finally
+        //     {
+        //         //this is where you would send notifications
+        //     }
+        }
+    }
+}
