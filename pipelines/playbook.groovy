cde5a9269b0b16202a7197fe4b47b64ec50254f3
dwarks123 <47684557+dwarks123@users.noreply.github.com>
2020-05-11 11:33:06 +0530

    Update playbook.groovy

--- pipelines/playbook.groovy
+++ pipelines/playbook.groovy
@@ -1,49 +1,39 @@
-def buildAgent = 'packer'
-def playbook = "ansiename.yaml"
+def buildAgent = 'CentosNode'
+def playbook = "EmpDeployPlaybook.yml"
+def ipdd
 
 timestamps
 {
     node(buildAgent)
     {  
-        // stage('Select Env And Invoke Playbook')
-        // {   
-        //     module = "${Envname}"
-        //     switch(module)
-        //     {
-        //         case "QA":  
-        //                     extraVars = "-e \"{'env':'${Envname}','uname':'${Username}'}\" "
-        //                     limit = "${Envname}"
-        //                     break;
-                            
-        //         case "DEV": 
-        //                     extraVars = "-e \"{'env':'${Envname}','uname':'${Username}'}\" "
-        //                     limit = "${Envname}"
-        //                     break;
-                            
-        //         case "PD":
-        //                     extraVars = "-e \"{'env':'${Envname}','uname':'${Username}'}\" "
-        //                     limit = ""
-                            
-        //                     break;
-        //     }
-        //     dir('ansible')
-        //     {
-        //         try{
-        //             ansiblePlaybook(
-        //             playbook: "$playbook",
-        //             extras: "$extraVars",
-        //             limit: "$limit")
-        //         }
-        //     }
-        //     catch (e)
-        //     {
-        //         echo e.getMessage()
-        //         currentBuild.result='FAILURE'
-        //     }   
-        //     finally
-        //     {
-        //         //this is where you would send notifications
-        //     }
-        
+        stage('Select Env And Invoke Playbook')
+        {  if(environment.contains('dev'))
+             {  ipdd = "192.168.0.28"
+                sh 'echo "$environment"'
+             }
+            else
+            {
+                ipdd = "192.168.0.31"
+                sh 'echo "$environment"'
+                
+            }
+                 
+            dir('ansible')
+             {   
+                try{
+                     ansiblePlaybook(
+                      playbook: "$playbook",
+                      inventory: "$ipdd,",
+                   //   extras: "$extraVars" 
+                      )
+                  } 
+                   
+                catch (e)
+                 {
+                    echo e.getMessage()
+                    currentBuild.result='FAILURE'
+                 }   
+            }
+        }
     }
 }
